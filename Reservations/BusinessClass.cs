dea0bc82887d8d84f97074be12f2de68e635a96d
Elahn <lightninglioness60@gmail.com>
2021-06-15 07:31:29 -0700

    Added data table as placeholder in BusinessClass.cs
    The placeholder data table will only be in this class, until I learn how to use ConsoleTableExt to build the actual data table.

--- Reservations/BusinessClass.cs
+++ Reservations/BusinessClass.cs
@@ -1,11 +1,24 @@
 using System;
+using System.Collections.Generic;
+using ConsoleTableExt;
 namespace AirConsoleApplication.Reservations
 {
    public class BusinessClass
    {
      public static void BusinessClassReservation()
-     {
-         
-     }
-   }
+        {
+          var tableData = new List<List<object>>
+          {
+            new List<object>{ "Sakura Yamamoto", "Support Engineer", "London", 46},
+            new List<object>{ "Serge Baldwin", "Data Coordinator", "San Francisco", 28, "something else" },
+            new List<object>{ "Shad Decker", "Regional Director", "Edinburgh"},
+          };
+        
+           ConsoleTableBuilder
+            .From(tableData)
+            .ExportAndWriteLine();
+
+
+        }
+    }
 }
